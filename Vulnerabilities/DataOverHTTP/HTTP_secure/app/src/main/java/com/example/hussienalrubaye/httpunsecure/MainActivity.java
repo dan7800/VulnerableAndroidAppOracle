837d2b5e04df6d4960a86a557ba995e97718bc1a
hussien alrubaye <hussien89aa@yahoo.com>
2016-03-16 23:26:17 -0400

    http insecure

---
+++ Vulnerabilities/DataOverHTTP/HTTP_secure/app/src/main/java/com/example/hussienalrubaye/httpunsecure/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.hussienalrubaye.httpunsecure;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buloginckic(View view) {
+        //get user name and password
+        EditText UserName=(EditText)findViewById(R.id.EDTUserName);
+        EditText Password=(EditText)findViewById(R.id.EDTpassword);
+        // send user name and password over the http
+        String url="http://sellingportal.alruabye.net/UsersWS.asmx/Login?UserName="+ cipher(UserName.getText().toString(),10) +"&Password="+ cipher(Password.getText().toString(),10);
+       // start background task
+        new MyAsyncTaskgetNews().execute(url, "news");
+    }
+
+
+    // get news from server
+    public class MyAsyncTaskgetNews extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            //before works
+        }
+        @Override
+        protected String  doInBackground(String... params) {
+            // TODO Auto-generated method stub
+            try {
+                String NewsData;
+                  //define the url we have to connect with
+                URL url = new URL(params[0]);
+                //make connect with url and send request
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                //waiting for 7000ms for response
+                urlConnection.setConnectTimeout(7000);//set timeout to 5 seconds
+
+                try {
+                    //getting the response data
+                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                    //convert the stream to string
+                    NewsData = ConvertInputToStringNoChange(in);
+                    //send to display data
+                    publishProgress(NewsData);
+                } finally {
+                    //end connection
+                    urlConnection.disconnect();
+                }
+
+            }catch (Exception ex){}
+            return null;
+        }
+        protected void onProgressUpdate(String... progress) {
+
+            try {
+                //display response data
+                Toast.makeText(getApplicationContext(),progress[0],Toast.LENGTH_LONG).show();
+
+            } catch (Exception ex) {
+            }
+
+
+        }
+
+        protected void onPostExecute(String  result2){
+
+
+        }
+
+
+
+
+    }
+
+// this method convert any stream to string
+    public static String ConvertInputToStringNoChange(InputStream inputStream) {
+
+        BufferedReader bureader=new BufferedReader( new InputStreamReader(inputStream));
+        String line ;
+        String linereultcal="";
+
+        try{
+            while((line=bureader.readLine())!=null) {
+
+                linereultcal+=line;
+
+            }
+            inputStream.close();
+
+
+        }catch (Exception ex){}
+
+        return linereultcal;
+    }
+
+    // cipher encryption add shift for key
+    public   String cipher(String msg, int shift) {
+        String s = "";
+        int len = msg.length(); // get string length
+        for (int x = 0; x < len; x++) {
+            char c = (char) (msg.charAt(x) + shift);  // shift every character
+            s += c; // append the characters
+        }
+        return s;
+    }
+}
