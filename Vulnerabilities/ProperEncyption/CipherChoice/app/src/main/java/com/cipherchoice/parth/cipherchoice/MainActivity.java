ad71b89cacf13cc68863e8a1ed7109d36045c560
Parth Sane <laerdevstudios@gmail.com>
2018-07-14 17:10:01 -0400

    Added New Cipher Activity Code

---
+++ Vulnerabilities/ProperEncyption/CipherChoice/app/src/main/java/com/cipherchoice/parth/cipherchoice/MainActivity.java
@@ -0,0 +1,220 @@
+package com.cipherchoice.parth.cipherchoice;
+//Code adopted from from Samfoundry.com, Rosettacode.org and howtodoinjava.com
+import android.graphics.Point;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+public class MainActivity extends AppCompatActivity {
+    private static char[][] charTable;
+    private static Point[] positions;
+    public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    private static SecretKeySpec secretKey;
+    private static byte[] key;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button encryptButton = findViewById(R.id.button);
+        encryptButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View view) {
+                EditText multiLineText = findViewById(R.id.editText2);
+                Spinner spinner = findViewById(R.id.spinner);
+                EditText text = findViewById(R.id.editText);
+
+                String key = "test";
+                String txt = text.getText().toString();
+                String jti = "y";
+                final boolean changeJtoI = jti.equalsIgnoreCase("y");
+                switch (spinner.getSelectedItem().toString()) {
+                    case "Easy - Caesar Cipher":
+
+                        multiLineText.setText(caesarEncrypt(txt, 3));
+                        break;
+                    case "Medium - Playfair Cipher":
+                        createTable(key, changeJtoI);
+                        String enc = encode(prepareText(txt, changeJtoI));
+                        multiLineText.setText(enc);
+                        break;
+                    case "Difficult - AES":
+                        multiLineText.setText(AES_Encrypt(txt, key));
+                        break;
+                }
+            }
+        });
+
+
+
+    }
+
+
+    private static String prepareText(String s, boolean changeJtoI) {
+        s = s.toUpperCase().replaceAll("[^A-Z]", "");
+        return changeJtoI ? s.replace("J", "I") : s.replace("Q", "");
+    }
+
+    private static void createTable(String key, boolean changeJtoI) {
+        charTable = new char[5][5];
+        positions = new Point[26];
+
+        String s = prepareText(key + "ABCDEFGHIJKLMNOPQRSTUVWXYZ", changeJtoI);
+
+        int len = s.length();
+        for (int i = 0, k = 0; i < len; i++) {
+            char c = s.charAt(i);
+            if (positions[c - 'A'] == null) {
+                charTable[k / 5][k % 5] = c;
+                positions[c - 'A'] = new Point(k % 5, k / 5);
+                k++;
+            }
+        }
+    }
+
+    //PLayfair Cipher Implementation
+    private static String encode(String s) {
+        StringBuilder sb = new StringBuilder(s);
+
+        for (int i = 0; i < sb.length(); i += 2) {
+
+            if (i == sb.length() - 1)
+                sb.append(sb.length() % 2 == 1 ? 'X' : "");
+
+            else if (sb.charAt(i) == sb.charAt(i + 1))
+                sb.insert(i + 1, 'X');
+        }
+        return codec(sb, 1);
+    }
+
+    private static String decode(String s) {
+        return codec(new StringBuilder(s), 4);
+    }
+
+    private static String codec(StringBuilder text, int direction) {
+        int len = text.length();
+        for (int i = 0; i < len; i += 2) {
+            char a = text.charAt(i);
+            char b = text.charAt(i + 1);
+
+            int row1 = positions[a - 'A'].y;
+            int row2 = positions[b - 'A'].y;
+            int col1 = positions[a - 'A'].x;
+            int col2 = positions[b - 'A'].x;
+
+            if (row1 == row2) {
+                col1 = (col1 + direction) % 5;
+                col2 = (col2 + direction) % 5;
+
+            } else if (col1 == col2) {
+                row1 = (row1 + direction) % 5;
+                row2 = (row2 + direction) % 5;
+
+            } else {
+                int tmp = col1;
+                col1 = col2;
+                col2 = tmp;
+            }
+
+            text.setCharAt(i, charTable[row1][col1]);
+            text.setCharAt(i + 1, charTable[row2][col2]);
+        }
+        return text.toString();
+    }
+
+    public static String caesarEncrypt(String plainText, int shiftKey)
+    {
+        plainText = plainText.toLowerCase();
+        String cipherText = "";
+        for (int i = 0; i < plainText.length(); i++)
+        {
+            int charPosition = ALPHABET.indexOf(plainText.charAt(i));
+            int keyVal = (shiftKey + charPosition) % 26;
+            char replaceVal = ALPHABET.charAt(keyVal);
+            cipherText += replaceVal;
+        }
+        return cipherText;
+    }
+
+    public static void setKey(String myKey)
+    {
+        MessageDigest sha = null;
+        try {
+            key = myKey.getBytes("UTF-8");
+            sha = MessageDigest.getInstance("SHA-1");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16);
+            secretKey = new SecretKeySpec(key, "AES");
+        }
+        catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static String AES_Encrypt(String strToEncrypt, String secret)
+    {
+        try
+        {
+            setKey(secret);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error while encrypting: " + e.toString());
+        }
+        return "Error while encrypting: ";
+    }
+
+
+    public static String PlayfairDecrypt(String cipherText, int shiftKey)
+    {
+        cipherText = cipherText.toLowerCase();
+        String plainText = "";
+        for (int i = 0; i < cipherText.length(); i++)
+        {
+            int charPosition = ALPHABET.indexOf(cipherText.charAt(i));
+            int keyVal = (charPosition - shiftKey) % 26;
+            if (keyVal < 0)
+            {
+                keyVal = ALPHABET.length() + keyVal;
+            }
+            char replaceVal = ALPHABET.charAt(keyVal);
+            plainText += replaceVal;
+        }
+        return plainText;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static String AES_Decrypt(String strToDecrypt, String secret)
+    {
+        try
+        {
+            setKey(secret);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error while decrypting: " + e.toString());
+        }
+        return null;
+    }
+}
